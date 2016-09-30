7fcc83cc33a89d6d27c077b648e50b3b7ca98e67
pangxb <10846295@qq.com>
2016-09-30 14:36:02 +0800

    yii2 mvc

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=yii2',
     'username' => 'root',
-    'password' => 'pang123',
+    'password' => 'password',
     'charset' => 'utf8',
 ];
